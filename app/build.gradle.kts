ee87b940c43cad14cdaa1c869b22e79b63a415c5
Marina Berdnikova <berdni4ka@rambler.ru>
2023-10-12 17:48:51 +0300

    Add messenger app

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -31,7 +31,8 @@ android {
 
 dependencies {
 
-    implementation("androidx.appcompat:appcompat:1.6.1")
+    //noinspection GradleCompatible
+    implementation("com.android.support:appcompat-v7:28.0.0")
     implementation("com.google.android.material:material:1.8.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     testImplementation("junit:junit:4.13.2")
@@ -41,4 +42,11 @@ dependencies {
     implementation("com.google.firebase:firebase-analytics")
 
     implementation("com.android.support:design:28.0.0")
+
+    implementation("com.google.firebase:firebase-auth:16.0.0")
+    implementation("com.google.firebase:firebase-database:16.0.0")
+    implementation("com.google.firebase:firebase-ui:0.6.2")
+
+
+
 }
--- app/src/main/java/com/example/messengerapp/MainActivity.java
+++ app/src/main/java/com/example/messengerapp/MainActivity.java
@@ -1,14 +1,50 @@
 package com.example.messengerapp;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.Snackbar;
+import android.widget.RelativeLayout;
+
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+import com.firebase.ui.auth.AuthUI;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static int SIGN_IN_CODE = 1;
+    private RelativeLayout activity_main;
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode == SIGN_IN_CODE){
+            if(resultCode == RESULT_OK){
+                Snackbar.make(activity_main, "Вы авторизованы", Snackbar.LENGTH_LONG).show();
+                displayAllMessages();
+            } else {
+                Snackbar.make(activity_main, "Вы не авторизованы", Snackbar.LENGTH_LONG).show();
+                finish();
+            }
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        activity_main = findViewById(R.id.activity_main);
+
+        if(FirebaseAuth.getInstance().getCurrentUser() == null)
+            startActivityForResult(AuthUI.getInstence().createSignInIntentBuilder().build(), SIGN_IN_CODE);
+        else
+            Snackbar.make(activity_main, "Вы авторизованы", Snackbar.LENGTH_LONG).show();
+
+        displayAllMessages();
+    }
+
+    private void displayAllMessages(){
+
     }
 }
